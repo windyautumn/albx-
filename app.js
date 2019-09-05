3351dcd061af167722206a198173ac81433ccd77
windyautumn <53567667+windyautumn@users.noreply.github.com>
2019-09-05 15:15:02 +0800

    设置默认引擎和托管静态资源

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ let express = require('express')
 
 
 let app = express()
-app.listen(3001,()=>{
-    console.log("http://127.0.0.1:3001")
+app.listen(3000,()=>{
+    console.log("http://127.0.0.1:3000")
 })
+// 托管静态资源
+app.use('/assets',express.static('assets'))
+app.use('/uploads',express.static('uploads'))
+//设置ejs引擎
 app.set('view engine','ejs')
-
+app.set('views',__dirname+'/views')
 
 
 app.get('/',(req,res)=>{
-    
+    res.render('index')
 })
