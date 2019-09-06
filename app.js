1e9bc091c33fb02376fc619b2b5c34eb66457676
windyautumn <53567667+windyautumn@users.noreply.github.com>
2019-09-06 19:02:40 +0800

    6-用session的方法完善登录

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express')
 let router = require('./router.js')
 let bodyParser = require('body-parser')
 let querystring = require('querystring')
+let session = require('express-session')
 
 let app = express()
 app.listen(3000,()=>{
@@ -15,10 +16,27 @@ app.use('/uploads',express.static('uploads'))
 //设置ejs引擎
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
+app.use(session({
+    secret:'abcdef',
+    resave:false,
+    saveUninitialized:false
+}))
+//用cookie的方法状态保持
+// app.use(function(req,res,next){
+// //  console.log(req.headers)
+//     let obj = querystring.parse(req.headers.cookie)
+//     if(obj.isLogin && obj.isLogin == 'true' || req.url =='/login' || req.url.indexOf('/admin')==-1){
+//         // console.log(1)
+//         next()
+//     }else{
+//         res.redirect('/login')
+//     }
+
+// })
+//用session的方法状态保持
 app.use(function(req,res,next){
 //  console.log(req.headers)
-    let obj = querystring.parse(req.headers.cookie)
-    if(obj.isLogin && obj.isLogin == 'true' || req.url =='/login' || req.url.indexOf('/admin')==-1){
+    if(req.session.isLogin && req.session.isLogin == 'true' || req.url =='/login' || req.url.indexOf('/admin')==-1){
         // console.log(1)
         next()
     }else{
--- contorller/userContorller.js
+++ contorller/userContorller.js
@@ -1,40 +1,45 @@
 let userModol = require('../modols/userModol')
 
-module.exports={
-    login(req,res){
+module.exports = {
+    login(req, res) {
         let obj = req.body
         let email = obj.email
         // console.log(email)
-        userModol.login(email,(err,data)=>{
-            if(err) {
+        userModol.login(email, (err, data) => {
+            if (err) {
                 res.json({
-                    code:400,
-                    des:'服务器异常'
+                    code: 400,
+                    des: '服务器异常'
                 })
-            }else{
-                if(data){
-                    if(data.password === obj.password){
+            } else {
+                if (data) {
+                    if (data.password === obj.password) {
                         // res.json({
                         //     code:200,
                         //     des:'登录成功'
                         // })
-                        res.writeHeader(200,{
-                            'Set-Cookie':'isLogin=true'
-                        })
+                        //用cookie来处理状态保持
+                        // res.writeHeader(200,{
+                        //     'Set-Cookie':'isLogin=true'
+                        // })
+                        //用session的方法处理状态保持
+                        req.session.isLogin = 'true'
+                        req.session.userDate = data
                         res.end(JSON.stringify({
-                            code:200,
-                            des:'登录成功'
+                            code: 200,
+                            des: '登录成功'
                         }))
-                    }else{
+
+                    } else {
                         res.json({
-                            code:400,
-                            des:'密码输入错误'
+                            code: 400,
+                            des: '密码输入错误'
                         })
                     }
-                }else{
+                } else {
                     res.json({
-                        code:400,
-                        des:'邮箱输入错误'
+                        code: 400,
+                        des: '邮箱输入错误'
                     })
                 }
             }
